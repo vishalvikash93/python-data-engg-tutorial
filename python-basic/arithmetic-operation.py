2cbb5082040c84b929affc0525d02a6906fb73e3
vishal <vishalvikash93@gmail.com>
2024-12-04 08:52:49 +0530

    initial commit

---
+++ python-basic/arithmetic-operation.py
@@ -0,0 +1,54 @@
+# +
+
+a =5
+b=20
+result=a+b
+print(result)
+
+
+#   -
+result=b-a
+print(result)
+
+#   *
+result=b*a
+print(result)
+
+#
+
+result=b/a
+print(result)
+print(type(result))
+
+# //  floor division
+x=21
+y=5
+result=x//y
+print(result)
+print(type(result))
+
+
+# ** power of number
+k=3
+j=2
+
+res=k**j
+print(res)
+
+
+
+#########################  comparison  operator ##########
+
+j=21
+i=20
+print(i==j)
+
+print(i!=j)
+
+print(j<i)
+print(j>i)
+
+print(j<=i)
+print(j>=i)
+
+
---
+++ python-basic/for-loop.py
@@ -0,0 +1,8 @@
+# lt=[2,3,5,1,6,3,7]
+
+
+# for elem in lt:
+#     print(elem)
+#
+#
+
